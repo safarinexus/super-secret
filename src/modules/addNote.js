a4692ce9c0410934e05bdee8072c20e74ac77af9
Edgar <edgargarteong@gmail.com>
2024-07-04 23:30:33 +0700

    added xinjie

--- src/modules/addNote.js
+++ src/modules/addNote.js
@@ -1,16 +1,21 @@
 export default function addNote(person, note, photo=false) {
+    const newID = (function replaceWhitespaceWithDash(str) {
+        return str.replace(/\s+/g, '-');
+    })(person);
+
+
     const wishes = document.querySelector(".wishes");
     const curr = document.createElement('div'); 
     curr.classList.add('card'); 
 
-    curr.id = person; 
+    curr.id = newID; 
     if (photo) {
-        curr.innerHTML = '<div class="image-slider" id="' + person +'-images"></div>\
-                        <div class="note" id="' + person + '-note">&quot;' + note + '&quot;</div>\
-                        <div class="from" id="' + person + '-from"> -from ' + person + '</div>';
+        curr.innerHTML = '<div class="image-slider" id="' + newID +'-images"></div>\
+                        <div class="note" id="' + newID + '-note">&quot;' + note + '&quot;</div>\
+                        <div class="from" id="' + newID + '-from"> -from ' + person + '</div>';
     } else {
-        curr.innerHTML = '<div class="note" id="' + person + '-note">&quot;' + note + '&quot;</div>\
-                        <div class="from" id="' + person + '-from"> -from ' + person + '</div>';
+        curr.innerHTML = '<div class="note" id="' + newID + '-note">&quot;' + note + '&quot;</div>\
+                        <div class="from" id="' + newID + '-from"> -from ' + person + '</div>';
     }
                         
     wishes.appendChild(curr);
