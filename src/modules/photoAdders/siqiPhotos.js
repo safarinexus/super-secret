062fe3b557787846ba3566821cc8eefb520211d0
Edgar <edgargarteong@gmail.com>
2024-07-05 03:59:28 +0700

    Revert "temporarily removed siqi vid"
    This reverts commit bd1511f72cb1e55f6616433e9eeec2d06d9c3410.

--- src/modules/photoAdders/siqiPhotos.js
+++ src/modules/photoAdders/siqiPhotos.js
@@ -1,3 +1,4 @@
+import siqi1 from '../../assets/siqiPhotos/siqiPhoto1.mp4';
 import siqi2 from '../../assets/siqiPhotos/siqiPhoto2.jpeg';
 import siqi3 from '../../assets/siqiPhotos/siqiPhoto3.mp4';
 import siqi4 from '../../assets/siqiPhotos/siqiPhoto4.jpeg';
@@ -11,6 +12,10 @@ import siqi9 from '../../assets/siqiPhotos/siqiPhoto9.jpeg';
 
 export default function siqiPhotos() {
     const slider = document.querySelector('#siqi-images'); 
+    const video1 = document.createElement('video');
+    video1.height = 240;
+    video1.src = siqi1;
+    video1.controls = true;
     const photo2 = new Image();
     photo2.src = siqi2; 
     photo2.style.height = '240px'; 
@@ -43,6 +48,7 @@ export default function siqiPhotos() {
     slider.appendChild(photo4); 
     slider.appendChild(photo5);
     slider.appendChild(photo6);
+    slider.appendChild(video1);
     slider.appendChild(photo7);
     slider.appendChild(photo8);
     slider.appendChild(photo9);
