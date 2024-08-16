27312b07bb1c956672a9b76615ba4cd3e1a33452
Edgar <edgargarteong@gmail.com>
2024-08-16 15:10:59 +0800

    changed to production

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js', 
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js', 
         path: path.resolve(__dirname, 'dist'),
